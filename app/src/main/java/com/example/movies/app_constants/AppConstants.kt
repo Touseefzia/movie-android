51d1731c8312b869ae28edcf533bc63b06e73e9d
Touseefzia <74051839+Touseefzia@users.noreply.github.com>
2024-05-13 09:52:16 +0500

    Update AppConstants.kt

--- app/src/main/java/com/example/movies/app_constants/AppConstants.kt
+++ app/src/main/java/com/example/movies/app_constants/AppConstants.kt
@@ -2,10 +2,4 @@ package com.example.movies.app_constants
 
 import androidx.compose.runtime.mutableStateOf
 
-object ApiPreferences {
-
-     var expandedType = mutableStateOf(false)
-
-
-     var totalScoreInDb = mutableStateOf("")
-}
+object ApiPreferences {}
